21b18a072f03e3c53c5aa6cfc65914732ecfbae5
Eunbin <kartrider_12@naver.com>
2022-06-26 02:04:53 +0900

    add spring config about kotest

---
+++ src/test/kotlin/hello/jdbc/SpringConfig.kt
@@ -0,0 +1,14 @@
+package hello.jdbc
+
+import io.kotest.core.config.AbstractProjectConfig
+import io.kotest.core.spec.IsolationMode
+import io.kotest.extensions.spring.SpringExtension
+
+object SpringConfig : AbstractProjectConfig() {
+    /* override val parallelism = 3
+     override val assertionMode = AssertionMode.Error
+     override val globalAssertSoftly = true
+     override val failOnIgnoredTests = false*/
+    override val isolationMode = IsolationMode.InstancePerLeaf
+    override fun extensions() = listOf(SpringExtension)
+}
